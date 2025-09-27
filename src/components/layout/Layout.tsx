c891f8a8c5138fb04d8fdd2b1b9793e501da7193
[dyad] <git@dyad.sh>
2025-09-28 00:10:49 +0200

    [dyad] Simplification du composant Layout pour déboguer l'erreur React.Children.only - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,27 +1,27 @@
 import React from "react";
-import Header from "./Header";
-import ImageCarousel from "@/components/ImageCarousel";
+// import Header from "./Header"; // Temporairement supprimé
+// import ImageCarousel from "@/components/ImageCarousel"; // Temporairement supprimé
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-const carouselImages = [
-  "/canneles-1.jpg",
-  "/canneles-2.jpg",
-  "/canneles-3.jpg",
-  "/canneles-4.jpg",
-  "/canneles-5.jpg",
-  "/canneles-6.jpg",
-  "/canneles-7.jpg",
-];
+// Les images du carrousel sont temporairement supprimées
+// const carouselImages = [
+//   "/canneles-1.jpg",
+//   "/canneles-2.jpg",
+//   "/canneles-3.jpg",
+//   "/canneles-4.jpg",
+//   "/canneles-5.jpg",
+//   "/canneles-6.jpg",
+//   "/canneles-7.jpg",
+// ];
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col relative"> {/* Ajout de 'relative' pour que le carrousel absolu se positionne par rapport à lui */}
-      <ImageCarousel images={carouselImages} /> {/* Déplacé ici pour couvrir toute la page */}
-      <Header /> {/* Le Header a déjà un z-index élevé et un fond */}
-      <main className="flex-grow relative z-10"> {/* Ajout de z-10 pour que le contenu soit au-dessus du carrousel */}
+    <div className="min-h-screen flex flex-col relative">
+      {/* ImageCarousel et Header sont temporairement supprimés */}
+      <main className="flex-grow relative z-10">
         {children}
       </main>
     </div>
