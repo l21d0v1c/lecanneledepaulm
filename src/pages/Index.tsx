cdbab1128c6166fb59abb59be63ad73d2ed8d05d
[dyad] <git@dyad.sh>
2025-09-27 23:10:44 +0200

    [dyad] Suppression du composant "Made with Dyad" - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,9 @@
-import { MadeWithDyad } from "@/components/made-with-dyad";
 import ProductList from "@/components/ProductList";
 
 const Index = () => {
   return (
     <div className="flex flex-col items-center bg-gray-50 dark:bg-gray-900 p-4 rounded-lg shadow-lg">
-      <div className="container mx-auto px-4"> {/* Ajout d'un conteneur pour le contenu principal */}
+      <div className="container mx-auto px-4">
         <div className="text-center mb-8 mt-4">
           <h1 className="text-4xl font-extrabold text-gray-900 dark:text-white mb-4">
             Découvrez nos Cannelés Artisanaux
@@ -15,7 +14,6 @@ const Index = () => {
         </div>
         <ProductList />
       </div>
-      <MadeWithDyad />
     </div>
   );
 };
