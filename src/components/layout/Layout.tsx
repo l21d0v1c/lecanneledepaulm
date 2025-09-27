f06afd1bd385fe72653a63b857dd6c4c9ffbbb33
[dyad] <git@dyad.sh>
2025-09-28 00:12:22 +0200

    [dyad] Réintégration progressive du Header pour déboguer l'erreur React.Children.only - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 import Header from "./Header";
-import ImageCarousel from "@/components/ImageCarousel";
+// import ImageCarousel from "@/components/ImageCarousel"; // Temporairement supprimé
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-const carouselImages = [
-  "/canneles-1.jpg",
-  "/canneles-2.jpg",
-  "/canneles-3.jpg",
-  "/canneles-4.jpg",
-  "/canneles-5.jpg",
-  "/canneles-6.jpg",
-  "/canneles-7.jpg",
-];
+// Les images du carrousel sont temporairement supprimées
+// const carouselImages = [
+//   "/canneles-1.jpg",
+//   "/canneles-2.jpg",
+//   "/canneles-3.jpg",
+//   "/canneles-4.jpg",
+//   "/canneles-5.jpg",
+//   "/canneles-6.jpg",
+//   "/canneles-7.jpg",
+// ];
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col relative">
-      <ImageCarousel images={carouselImages} />
+      {/* ImageCarousel est temporairement supprimé */}
       <Header />
       <main className="flex-grow relative z-10">
         {children}
