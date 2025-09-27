422841efa818d59439826616fb04764030351c44
[dyad] <git@dyad.sh>
2025-09-28 00:13:37 +0200

    [dyad] Réintégration du carrousel et correction du lien du titre dans le Header - wrote 2 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,26 +1,25 @@
 import React from "react";
 import Header from "./Header";
-// import ImageCarousel from "@/components/ImageCarousel"; // Temporairement supprimé
+import ImageCarousel from "@/components/ImageCarousel";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-// Les images du carrousel sont temporairement supprimées
-// const carouselImages = [
-//   "/canneles-1.jpg",
-//   "/canneles-2.jpg",
-//   "/canneles-3.jpg",
-//   "/canneles-4.jpg",
-//   "/canneles-5.jpg",
-//   "/canneles-6.jpg",
-//   "/canneles-7.jpg",
-// ];
+const carouselImages = [
+  "/canneles-1.jpg",
+  "/canneles-2.jpg",
+  "/canneles-3.jpg",
+  "/canneles-4.jpg",
+  "/canneles-5.jpg",
+  "/canneles-6.jpg",
+  "/canneles-7.jpg",
+];
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col relative">
-      {/* ImageCarousel est temporairement supprimé */}
+      <ImageCarousel images={carouselImages} />
       <Header />
       <main className="flex-grow relative z-10">
         {children}
