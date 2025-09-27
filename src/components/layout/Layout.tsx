afe94382f8b1c50da483ea867be21fe7e7e5ce1b
[dyad] <git@dyad.sh>
2025-09-27 23:44:25 +0200

    [dyad] Le carrousel d'images couvre maintenant toute la page - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -12,19 +12,17 @@ const carouselImages = [
   "/canneles-3.jpg",
   "/canneles-4.jpg",
   "/canneles-5.jpg",
-  "/canneles-6.jpg", // Anciennement canneles-7.jpg
-  "/canneles-7.jpg", // Anciennement canneles-8.jpg
+  "/canneles-6.jpg",
+  "/canneles-7.jpg",
 ];
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow relative min-h-[calc(100vh-4rem)]">
-        <ImageCarousel images={carouselImages} />
-        <div className="relative z-10 h-full w-full flex flex-col items-center justify-center">
-          {children}
-        </div>
+    <div className="min-h-screen flex flex-col relative"> {/* Ajout de 'relative' pour que le carrousel absolu se positionne par rapport à lui */}
+      <ImageCarousel images={carouselImages} /> {/* Déplacé ici pour couvrir toute la page */}
+      <Header /> {/* Le Header a déjà un z-index élevé et un fond */}
+      <main className="flex-grow relative z-10"> {/* Ajout de z-10 pour que le contenu soit au-dessus du carrousel */}
+        {children}
       </main>
     </div>
   );
