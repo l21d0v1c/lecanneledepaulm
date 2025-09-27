aab75d815b93e0cc99f321527b7f6e619d56b67a
[dyad] <git@dyad.sh>
2025-09-28 00:07:41 +0200

    [dyad] Débogage de l'erreur React.Children.only en commentant le TooltipProvider - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+// import { TooltipProvider } from "@/components/ui/tooltip"; // Commenté pour le débogage
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Layout from "./components/layout/Layout";
-import React from "react"; // Import de React pour utiliser React.Fragment
+import React from "react";
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  // L'App retourne un seul React.Fragment
   <React.Fragment>
-    {/* QueryClientProvider enveloppe le TooltipProvider */}
     <QueryClientProvider client={queryClient}>
-      {/* TooltipProvider enveloppe le BrowserRouter */}
-      <TooltipProvider>
-        {/* BrowserRouter enveloppe le Layout */}
+      {/* TooltipProvider est temporairement commenté pour le débogage */}
+      {/* <TooltipProvider> */}
         <BrowserRouter>
           <Layout>
             <Routes>
@@ -27,10 +24,8 @@ const App = () => (
             </Routes>
           </Layout>
         </BrowserRouter>
-      </TooltipProvider>
+      {/* </TooltipProvider> */}
     </QueryClientProvider>
-    {/* Toaster et Sonner sont des frères et sœurs directs du QueryClientProvider,
-        au niveau le plus élevé de l'application, à l'intérieur du React.Fragment. */}
     <Toaster />
     <Sonner />
   </React.Fragment>
