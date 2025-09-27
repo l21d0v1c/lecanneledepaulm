afaa895cf8da00f5279b74b3505d4af55b9bbf1b
[dyad] <git@dyad.sh>
2025-09-28 00:50:09 +0200

    [dyad] Définition d'une hauteur fixe pour la barre de navigation - wrote 1 file(s)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,8 +6,8 @@ import OvalButton from "../OvalButton";
 
 const Header: React.FC = () => {
   return (
-    <header className="sticky top-0 z-40 w-full border-b border-black bg-transparent">
-      <div className="flex w-full items-center justify-between px-4 py-6"> {/* Ajustement du padding vertical à py-6 */}
+    <header className="sticky top-0 z-40 w-full border-b border-black bg-transparent h-20"> {/* Hauteur fixe h-20 */}
+      <div className="flex w-full items-center justify-between px-4 h-full"> {/* Supprime py-6, utilise h-full et items-center */}
         <div className="flex-1"></div>
         <OvalButton>Le Cannelé de Paul M</OvalButton>
         <div className="flex-1 flex justify-end">
