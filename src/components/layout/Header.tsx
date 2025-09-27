fffe75d9529689786a861287965e83e17ffd9d39
[dyad] <git@dyad.sh>
2025-09-28 01:22:07 +0200

    [dyad] Suppression de la classe flex du conteneur principal de l'en-tête - wrote 1 file(s)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,10 +7,10 @@ import OvalButton from "../OvalButton";
 const Header: React.FC = () => {
   return (
     <header className="sticky top-0 z-40 w-full border-b border-black bg-transparent h-56 md:h-72">
-      <div className="flex w-full items-center justify-between px-4">
+      <div className="w-full items-center justify-between px-4"> {/* 'flex' a été retiré ici */}
         <div className="flex-1"></div>
         <OvalButton>Le Cannelé de Paul M</OvalButton>
-        <div className="flex-1 justify-end"> {/* 'flex' a été retiré ici */}
+        <div className="flex-1 justify-end">
           <nav>
             <Button variant="ghost" size="icon" aria-label="Panier" className="text-white hover:bg-white/20">
               <ShoppingCart className="h-8 w-8" />
