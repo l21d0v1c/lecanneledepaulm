ef32473bce9200b6c9cd50ecd57ff5dbd4323084
[dyad] <git@dyad.sh>
2025-09-28 11:19:25 +0200

    [dyad] Ajustement de la largeur des boutons de la barre de navigation secondaire - wrote 1 file(s)

--- src/components/layout/SecondaryNavbar.tsx
+++ src/components/layout/SecondaryNavbar.tsx
@@ -3,24 +3,24 @@ import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
 const SecondaryNavbar: React.FC = () => {
-  // Le bouton prendra toute la largeur de sa colonne de grille
-  const buttonStyle = "bg-black text-white border-2 border-[#f8ac54] hover:bg-gray-800 rounded-full text-sm py-2 px-4 whitespace-nowrap flex-shrink-0 w-full text-center";
+  // Suppression de 'w-full' pour que les boutons soient moins larges
+  const buttonStyle = "bg-black text-white border-2 border-[#f8ac54] hover:bg-gray-800 rounded-full text-sm py-2 px-4 whitespace-nowrap flex-shrink-0 text-center";
 
   return (
     <nav className="sticky top-32 w-full z-40 border-b border-white bg-transparent">
       {/* Utilisation d'une grille à 3 colonnes pour une répartition égale */}
       <div className="container grid grid-cols-3 gap-4 h-16 items-center px-4">
-        <Link to="/sales" className="col-span-1"> {/* Chaque lien occupe une colonne */}
+        <Link to="/sales" className="col-span-1 flex justify-center"> {/* Ajout de flex justify-center pour centrer le bouton */}
           <Button className={buttonStyle}>
             Salés
           </Button>
         </Link>
-        <Link to="/sucres" className="col-span-1">
+        <Link to="/sucres" className="col-span-1 flex justify-center"> {/* Ajout de flex justify-center */}
           <Button className={buttonStyle}>
             Sucrés
           </Button>
         </Link>
-        <Link to="/creations" className="col-span-1">
+        <Link to="/creations" className="col-span-1 flex justify-center"> {/* Ajout de flex justify-center */}
           <Button className={buttonStyle}>
             Créations
           </Button>
