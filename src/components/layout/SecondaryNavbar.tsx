d173cd9ee9feeaa50b6dbeb36a2aae064e88448b
[dyad] <git@dyad.sh>
2025-09-28 11:18:48 +0200

    [dyad] Uniformisation et réactivité des boutons de la barre de navigation secondaire - wrote 1 file(s)

--- src/components/layout/SecondaryNavbar.tsx
+++ src/components/layout/SecondaryNavbar.tsx
@@ -3,23 +3,24 @@ import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
 const SecondaryNavbar: React.FC = () => {
-  // Ajout de 'w-full' pour que le bouton remplisse la largeur de son parent Link
-  const buttonStyle = "bg-black text-white border-2 border-[#f8ac54] hover:bg-gray-800 rounded-full text-sm py-2 px-4 whitespace-nowrap flex-shrink-0 w-full";
+  // Le bouton prendra toute la largeur de sa colonne de grille
+  const buttonStyle = "bg-black text-white border-2 border-[#f8ac54] hover:bg-gray-800 rounded-full text-sm py-2 px-4 whitespace-nowrap flex-shrink-0 w-full text-center";
 
   return (
     <nav className="sticky top-32 w-full z-40 border-b border-white bg-transparent">
-      <div className="container flex h-16 items-center justify-around px-4">
-        <Link to="/sales" className="flex-1"> {/* Ajout de flex-1 pour que chaque lien prenne un espace égal */}
+      {/* Utilisation d'une grille à 3 colonnes pour une répartition égale */}
+      <div className="container grid grid-cols-3 gap-4 h-16 items-center px-4">
+        <Link to="/sales" className="col-span-1"> {/* Chaque lien occupe une colonne */}
           <Button className={buttonStyle}>
             Salés
           </Button>
         </Link>
-        <Link to="/sucres" className="flex-1"> {/* Ajout de flex-1 */}
+        <Link to="/sucres" className="col-span-1">
           <Button className={buttonStyle}>
             Sucrés
           </Button>
         </Link>
-        <Link to="/creations" className="flex-1"> {/* Ajout de flex-1 */}
+        <Link to="/creations" className="col-span-1">
           <Button className={buttonStyle}>
             Créations
           </Button>
