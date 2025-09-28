cb112caa56cc55aac0ffc516f4477b5968508262
[dyad] <git@dyad.sh>
2025-09-28 05:11:42 +0200

    [dyad] Ajout d'un dégradé pour masquer le contenu sous la navbar transparente - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -17,4 +17,19 @@ const carouselImages = [
   "/canneles-7.jpg",
 ];
 
-const Layout: React.FC<LayoutProps> = ({ children }) => { return ( <div className="min-h-screen flex flex-col relative"> <ImageCarousel images={carouselImages} /> {/* z-[-1] */} <Header /> {/* z-40 */} <SecondaryNavbar /> {/* z-30 */} <main className="flex-grow relative z-10 pt-12"> {/* pt-12 */} {children} </main> </div> ); }; export default Layout;
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col relative">
+      <ImageCarousel images={carouselImages} />
+      {/* Dégradé progressif pour masquer le contenu sous la navbar */}
+      <div className="fixed top-0 left-0 right-0 h-48 bg-gradient-to-b from-black/70 to-transparent pointer-events-none z-20" />
+      <Header />
+      <SecondaryNavbar />
+      <main className="flex-grow relative z-10 pt-12">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
