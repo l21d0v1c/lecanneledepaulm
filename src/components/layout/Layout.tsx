77f8619f40575338cf539c06d69922eb16722499
[dyad] <git@dyad.sh>
2025-09-28 05:20:45 +0200

    [dyad] Suppression du masque et du dégradé - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -21,8 +21,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col relative">
       <ImageCarousel images={carouselImages} />
-      {/* Dégradé progressif renforcé pour masquer le contenu sous la navbar */}
-      <div className="fixed top-0 left-0 right-0 h-48 bg-gradient-to-b from-black to-black/90 pointer-events-none z-30" />
       <Header />
       <SecondaryNavbar />
       <main className="flex-grow relative z-10 pt-12">
