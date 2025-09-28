427acf7fefc65b6529f0bbfebe38efa096f17a42
[dyad] <git@dyad.sh>
2025-09-28 03:13:52 +0200

    [dyad] Ajout d'un espace entre la barre de navigation et le contenu - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -23,7 +23,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <ImageCarousel images={carouselImages} /> {/* z-[-1] */}
       <Header /> {/* z-40 */}
       <SecondaryNavbar /> {/* z-30 */}
-      <main className="flex-grow relative z-10 pt-37"> {/* bg-background a été retiré pour rendre le contenu transparent, pt-40 changé en pt-37 */}
+      <main className="flex-grow relative z-10 pt-40"> {/* Augmenté le padding-top pour plus d'espace */}
         {children}
       </main>
     </div>
