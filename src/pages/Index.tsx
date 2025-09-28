eaff040be679e17bf074529e0810844b5a348af1
[dyad] <git@dyad.sh>
2025-09-28 02:52:01 +0200

    [dyad] Rétablissement de la transparence complète et correction de l'espacement - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ const Index = () => {
   return (
     <div className="relative w-full flex flex-col items-center justify-center">
       {/* Section du titre et de la description - reste transparente sur le carrousel */}
-      <div className="relative z-10 w-full max-w-4xl p-4 container mx-auto py-8 pt-[12rem]">
+      <div className="relative z-10 w-full max-w-4xl p-4 container mx-auto py-8"> {/* Suppression de pt-[12rem] */}
         <div className="text-center mb-8 mt-4">
           <h1 className="text-4xl font-extrabold text-white mb-4">
             Découvrez nos Cannelés Artisanaux
@@ -13,13 +13,8 @@ const Index = () => {
             Chaque bouchée, un voyage gustatif.
           </p>
         </div>
-      </div>
-
-      {/* Nouveau conteneur pour ProductList avec un fond opaque */}
-      <div className="relative z-10 w-full bg-background py-8 px-4"> {/* Ajout de bg-background ici */}
-        <div className="container mx-auto max-w-4xl">
-          <ProductList />
-        </div>
+        {/* ProductList est maintenant directement ici, sans conteneur opaque */}
+        <ProductList />
       </div>
     </div>
   );
