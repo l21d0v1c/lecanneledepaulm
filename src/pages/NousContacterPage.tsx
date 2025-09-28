7995b427f4fc5c98c04dd4397e209809259f72ee
[dyad] <git@dyad.sh>
2025-09-28 12:58:23 +0200

    [dyad] Mise à jour du titre et du contenu de la page 'Nous Contacter' - wrote 1 file(s)

--- src/pages/NousContacterPage.tsx
+++ src/pages/NousContacterPage.tsx
@@ -7,16 +7,16 @@ const NousContacterPage: React.FC = () => {
       <Card className="flex flex-col bg-transparent shadow-none border border-white h-full w-full max-w-2xl">
         <CardHeader className="p-6 text-center">
           <CardTitle className="text-4xl md:text-5xl font-extrabold text-black mb-4 text-shadow-cannelé">
-            Nous Contacter
+            06.76.79.55.10
           </CardTitle>
         </CardHeader>
         <CardContent className="flex-grow flex flex-col justify-center p-6 text-center">
           <p className="mb-6 text-xl md:text-2xl text-black text-shadow-cannelé leading-relaxed">
             Pour toute question, commande spéciale ou simplement pour discuter de nos délicieux cannelés, n'hésitez pas à nous contacter !
             <br /><br />
-            Vous pouvez nous joindre par téléphone au : **[Votre numéro de téléphone]**
+            Vous pouvez nous joindre par téléphone au : **06.76.79.55.10**
             <br />
-            Ou par email à : **[Votre adresse email]**
+            Ou par email à : **contact@lecannaledepaulm.fr**
             <br /><br />
             Nous sommes impatients de vous entendre !
           </p>
