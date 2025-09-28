654dfa1828af907167e0d6ff243908d00b186c3a
[dyad] <git@dyad.sh>
2025-09-28 03:05:02 +0200

    [dyad] Correction du chevauchement du contenu sous les navbars - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -20,10 +20,12 @@ const carouselImages = [
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col relative">
-      <ImageCarousel images={carouselImages} />
-      <Header />
-      <SecondaryNavbar />
-      <main className="flex-grow relative z-10"> {/* Suppression de bg-background */}
+      <ImageCarousel images={carouselImages} /> {/* z-[-1] */}
+      {/* Couche opaque fixe pour masquer le contenu sous les navbars transparentes */}
+      <div className="fixed top-0 left-0 w-full h-48 bg-black z-20"></div> {/* h-48 = h-32 (Header) + h-16 (SecondaryNavbar) */}
+      <Header /> {/* z-40 */}
+      <SecondaryNavbar /> {/* z-30 */}
+      <main className="flex-grow relative z-10 pt-48"> {/* Ajout de pt-48 pour décaler le contenu */}
         {children}
       </main>
     </div>
